321b3d3f34a80a05a946a70cf28f6fe89c2b956d
LilGast8 <gastonrider73number8@gmail.com>
2014-06-19 19:54:53 +0200

    v1.0.0 - 13-01-10

---
+++ www/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>[TITLE]</title>
+    <meta charset="utf-8" />
+    <meta name="author" content="Gaston Bouchayer" />
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <meta name="robots" content="index, follow">
+    <noscript><meta http-equiv="refresh" content="0; url=no_js.php"></noscript>
+
+    <meta property="og:title" content="" />
+    <meta property="og:site_name" content="" />
+    <meta property="og:description" content="" />
+    <meta property="og:url" content="" />
+    <meta property="og:image" content="" />
+    <meta property="og:type" content="website" />
+
+    <link rel="shortcut icon" href="images/logos/favicon.ico" />
+    <link rel="apple-touch-icon-precomposed" href="images/logos/logo-apple-touch.jpg" />
+
+    <link media="screen" rel="stylesheet" type="text/css" href="css/styles.css" />
+    
+    <!--[if lt IE 9]><script type="text/javascript" src="js/html5shiv.js"></script><![endif]-->
+    <script src="js/browser_detect.js"></script>
+</head>
+
+<!--[if IE 7]><body class="ie7 oldie"><![endif]-->
+<!--[if IE 8]><body class="ie8 oldie"><![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><body><!--<![endif]-->
+
+
+
+
+
+<!-- Scripts -->
+<script src="js/jquery.min.js"></script>
+<script src="js/greensock/TweenMax.min.js"></script>
+<script src="js/jquery.address.min.js"></script>
+<script src="js/scripts.js"></script>
+
+<?php if($_SERVER['HTTP_HOST'] != 'localhost:8888') { ?>
+<!-- Google Analytics -->
+<script>
+    var _gaq=[['_setAccount','UA-XXXX-XX'],['_trackPageview']];(function(a,b){var c=a.createElement(b),d=a.getElementsByTagName(b)[0];c.src=("https:"==location.protocol?"//ssl":"//www")+".google-analytics.com/ga.js";d.parentNode.insertBefore(c,d)})(document,"script");
+</script>
+
+<?php } ?>
+
+</body>
+</html>
---
+++ www/old_browser.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<title>[TITLE]</title>
+    <meta charset="utf-8" />
+    <meta name="author" content="Gaston Bouchayer" />
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <meta name="robots" content="noindex, nofollow">
+
+    <link rel="shortcut icon" href="images/divers/favicon.ico" />
+	<link media="screen" rel="stylesheet" type="text/css" href="css/styles.css" />
+
+    <script src="js/browser_detect.js"></script>
+</head>
+
+<body>
+
+
+<div id="oldBrowser">
+	<h1>Votre navigateur est obsolète.</h1>
+    <p>
+	    Veuillez le mettre à jour gratuitement, afin d'améliorer ses performances et <br />
+	    ainsi avoir un meilleur confort de navigation.
+    </p>
+    <div class="navigateurs">
+    	<a href="http://www.google.com/chrome" target="_blank" class="chrome">
+        	<h2>Chrome</h2>
+            <p>Télécharger</p>
+        </a>
+        <a href="http://www.mozilla.org/firefox" target="_blank" class="firefox">
+        	<h2>Firefox</h2>
+            <p>Télécharger</p>
+        </a>
+        <a href="http://www.apple.com/fr/safari" target="_blank" class="safari">
+        	<h2>Safari</h2>
+            <p>Télécharger</p>
+        </a>
+        <a href="http://www.opera.com/download" target="_blank" class="opera">
+        	<h2>Opera</h2>
+            <p>Télécharger</p>
+        </a>
+        <a href="http://www.microsoft.com/france/windows/internet-explorer" target="_blank" class="ie">
+        	<h2>Internet Explorer</h2>
+            <p>Télécharger</p>
+        </a>
+    </div>
+</div>
+
+
+</body>
+</html>
