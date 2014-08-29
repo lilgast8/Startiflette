124926092e3470111ebdcd3ec209778397e3842c
LilGast8 <contact@gastonbouchayer.com>
2014-08-29 12:24:53 +0200

    [3.1.4] Change jsonlint gulp plugin.

--- gulp/tasks/json-lint.js
+++ gulp/tasks/json-lint.js
@@ -3,19 +3,19 @@ var gulp = require('gulp');
 var paths = require('../utils/paths');
 var plumber = require('gulp-plumber');
 var notify = require('gulp-notify');
-var jsonlint = require('gulp-json-lint');
+var jsonlint = require('gulp-jsonlint');
 
 
 gulp.task('json-lint', function() {
 	
-	gulp.src(paths.src.json+'**/*.json')
+	gulp.src([paths.src.json+'**/*.json', paths.src.js+'js-files.json'])
 		.pipe(plumber())
 		.pipe(jsonlint())
-		.pipe(jsonlint.report('verbose'))
-		.pipe(jsonlint.report(function(lint, file) {
-			var posJsonStr = file.path.indexOf('json/')+5;
+		.pipe(jsonlint.reporter())
+		.pipe(jsonlint.reporter(function(file) {
+			var posJsonStr = file.path.indexOf('www/')+4;
 			var fileName = file.path.substring(posJsonStr, file.path.length);
-			return notify().write(fileName+' ('+lint.line+':'+lint.character+')\n'+lint.error);
+			return notify().write('JSON Lint error on: '+fileName);
 		}));
 	
 });
