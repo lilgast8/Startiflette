98be55b732dcb3e25aca726e3b3366b30c5702bd
LilGast8 <contact@gastonbouchayer.com>
2014-11-24 22:59:46 +0100

    Utils.Global.js : method added or rewritten.

--- www/src/js/app/utils/Utils.Global.js
+++ www/src/js/app/utils/Utils.Global.js
@@ -9,41 +9,101 @@ APP.Utils.Global = (function(window){
 	this.color = {};
 	
 	
-	this.removeClass = function(elt, classToRemove) {
-		elt.className = elt.className.replace(new RegExp('(^|\\b)' + classToRemove.split(' ').join('|') + '(\\b|$)', 'gi'), '');
-		
-		var lastCharPos = elt.className.length-1;
-		if(elt.className[lastCharPos] == ' ')
-			elt.className = elt.className.substring(0, lastCharPos);
+	this.addClass = function(elt, classToAdd){
+		if(elt.classList)
+			elt.classList.add(classToAdd);
+		else
+			elt.className += ' ' + classToAdd;
 	};
 	
 	
-	this.getImageSize = function(imgW, imgH, contW, contH) {
-		var imgRatio = imgW/imgH;
+	this.removeClass = function(elt, classToRemove) {
+		if(elt.classList)
+			elt.classList.remove(classToRemove);
+		else {
+			elt.className = elt.className.replace(new RegExp('(^|\\b)' + classToRemove.split(' ').join('|') + '(\\b|$)', 'gi'), '');
+			
+			var lastCharPos = elt.className.length-1;
+			if(elt.className[lastCharPos] == ' ')
+				elt.className = elt.className.substring(0, lastCharPos);
+		}
+	};
+	
+	
+	this.hasClass = function(elt, classToCheck) {
+		var hasClass;
+		
+		if(elt.classList)
+			hasClass = elt.classList.contains(classToCheck);
+		else
+			hasClass = new RegExp('(^| )' + classToCheck + '( |$)', 'gi').test(elt.className);
+		
+		return hasClass;
+	};
+	
+	
+	this.resetStyle = function(elt){
+		elt.style.cssText = '';
+	};
+	
+	
+	this.getEltSize = function(eltW, eltH, contW, contH) {
+		var imgRatio = eltW/eltW;
 		var contRatio = contW/contH;
-		var sizeImg = {
+		var sizeElt = {
 			x : 0,
 			y : 0,
 			w : 0,
 			h : 0
 		};
 		
-		if(imgRatio < contRatio) {
-			sizeImg.w = contW;
-			sizeImg.h = Math.round(sizeImg.w/imgRatio);
-			sizeImg.y = Math.round(-(sizeImg.h-contH)/2);
+		if(eltRatio < contRatio) {
+			sizeElt.w = contW;
+			sizeElt.h = Math.round(sizeElt.w/eltRatio);
+			sizeElt.y = Math.round(-(sizeElt.h-contH)/2);
 		} else {
-			sizeImg.h = contH;
-			sizeImg.w = Math.round(sizeImg.h*imgRatio);
-			sizeImg.x = Math.round(-(sizeImg.w-contW)/2);
+			sizeElt.h = contH;
+			sizeElt.w = Math.round(sizeElt.h*eltRatio);
+			sizeElt.x = Math.round(-(sizeElt.w-contW)/2);
 		}
 		
-		return sizeImg;
+		return sizeElt;
 	};
 	
 	
 	this.degToRad = function(deg) {
 		return deg*Math.PI/180;
+	};
+	
+	
+	this.radToDeg = function(rad) {
+		return rad*180/Math.PI;
+	};
+	
+	
+	this.getSupportedPropertyName = function(property) {
+		var prefixes = ['', 'ms', 'Webkit', 'Moz', 'O'];
+		
+		for(var i = 0; i < prefixes.length; i++) {
+			var prefix = prefixes[i];
+			property = prefix === '' ? property : property.charAt(0).toUpperCase() + property.substring(1).toLowerCase();
+			var prop = prefix+property;
+			
+			if(typeof document.body.style[prop] != "undefined")
+				return prop;
+		}
+		
+		return null;
+	};
+	
+	
+	this.initMailto = function(elt, address, domain, end) {
+		var mailto = 'mailto';
+		var separator = ':';
+		var at = '@';
+		var dot = '.';
+		
+		elt.href = mailto+separator+address+at+domain+dot+end;
 	};
 	
 	
