6a81795f8adde8c8535dbc5530d2eb7ad450b58f
LilGast8 <contact@gastonbouchayer.com>
2014-12-04 18:01:42 +0100

    OldBrowser.js

--- www/src/js/shared/config/OldBrowser.js
+++ www/src/js/shared/config/OldBrowser.js
@@ -9,14 +9,14 @@ APP.OldBrowser = (function(window) {
 	
 	
 	OldBrowser.prototype.init = function() {
-		var br = APP.Config.BROWSER;
-		var brV = parseInt(APP.Config.BROWSER_VERSION);
+		var browser = APP.Config.BROWSER;
+		var browserVersion = _getBrowserVersion.call(this, APP.Config.BROWSER_VERSION);
 		
-		if( br == 'ie' && brV < 9 || 
-			br == 'firefox' && brV < 10 || 
-			br == 'opera' && brV < 11 || 
-			br == 'safari' && brV < 4 || 
-			br == 'chrome' && brV < 17) {
+		if(browser == 'ie' && browserVersion < 9 || 
+			browser == 'firefox' && browserVersion < 10 || 
+			browser == 'opera' && browserVersion < 11 || 
+			browser == 'safari' && browserVersion < 5 || 
+			browser == 'chrome' && browserVersion < 17) {
 			
 			$.ajax({
 				url: APP.Config.WEB_ROOT+APP.Config.LG+'/old-browser',
@@ -26,6 +26,23 @@ APP.OldBrowser = (function(window) {
 				error: _errorAjax.bind(this)
 			});
 		}
+	};
+	
+	
+	var _getBrowserVersion = function(browserVersion) {
+		var firstPointPos = browserVersion.indexOf('.');
+		
+		var aBrowserVersion = APP.Config.BROWSER_VERSION.split('.');
+		
+		browserVersion = '';
+		
+		for(var i=0; i<aBrowserVersion.length; i++) {
+			browserVersion += aBrowserVersion[i];
+		}
+		
+		browserVersion = parseFloat( browserVersion.slice(0, firstPointPos) + '.' + browserVersion.slice(firstPointPos, browserVersion.length) );
+		
+		return browserVersion;
 	};
 	
 	
