13708028587e09afd92f2da94215d631485d747b
LilGast8 <contact@gastonbouchayer.com>
2014-12-28 02:20:43 +0100

    LANG & ALL_LANG removed from footer and completely managed in Config.js

--- www/php/shared/js-var.php
+++ www/php/shared/js-var.php
@@ -4,6 +4,4 @@
 	var LOCALHOST	= '<?php echo Config::$LOCALHOST; ?>';
 	var PROD		= '<?php echo Config::PROD; ?>';
 	var WEB_ROOT	= '<?php echo $main->path->url->base; ?>';
-	var LANG		= '<?php echo Config::$LANG; ?>';
-	var ALL_LANG	= <?php echo json_encode(Config::$ALL_LANG); /* [<?php echo '"'.implode('","', $allLg).'"' ?>]; */ ?>;
 </script>
--- www/src/js/app/shared/config/Config.js
+++ www/src/js/app/shared/config/Config.js
@@ -7,9 +7,9 @@ APP.Config = (function(window) {
 		this.LOCALHOST			= LOCALHOST == '1' ? true : false;
 		this.PROD				= PROD == '1' ? true : false;
 		this.WEB_ROOT			= WEB_ROOT;
-		this.LANG				= LANG;
-		this.MULTI_LANG			= ALL_LANG.length == 1 ? false : true;
-		this.ALL_LANG			= ALL_LANG;
+		this.LANG				= null;
+		this.MULTI_LANG			= null;
+		this.ALL_LANG			= null;
 		
 		this.BROWSER			= null;
 		this.BROWSER_VERSION	= null;
@@ -28,9 +28,38 @@ APP.Config = (function(window) {
 		this.IS_IE				= APP.Config.BROWSER == 'ie' ? true : false;
 		this.IE_VERSION			= null;
 		
+		this.ALL_LANG			= _setAllLang.call(this);
+		this.MULTI_LANG			= this.ALL_LANG.length == 1 ? false : true;
+		this.LANG				= _setLang.call(this);
+		
 		this.HAS_PUSHSTATE		= Modernizr.history;
 		
 		APP.OldBrowser.init();
+	};
+	
+	
+	var _setAllLang = function() {
+		var allLang = [];
+		
+		for(var lang in APP.Model.Global.json.pages)
+			allLang.push(lang);
+		
+		return allLang;
+	};
+	
+	
+	var _setLang = function() {
+		var lang;
+		
+		var current = History.getState().url;
+		var pageUrl = current.replace(this.WEB_ROOT, '');
+		
+		if(!this.MULTI_LANG || pageUrl.length === 0)
+			lang = this.ALL_LANG[0];	
+		else
+			lang = pageUrl.substr(0, 2);
+		
+		return lang;
 	};
 	
 	
