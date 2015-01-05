e753d50ca5255e2099572009000c58c721c69be4
LilGast8 <contact@gastonbouchayer.com>
2015-01-05 23:02:03 +0100

    favicons.php : APP_NAME added

--- www/php/shared/favicons.php
+++ www/php/shared/favicons.php
@@ -1,4 +1,4 @@
-
+	
 	<!-- Favicons -->
 	<link rel="apple-touch-icon" sizes="57x57" href="<?php echo $main->path->url->img; ?>logos/favicons/apple-touch-icon-57x57.png">
 	<link rel="apple-touch-icon" sizes="114x114" href="<?php echo $main->path->url->img; ?>logos/favicons/apple-touch-icon-114x114.png">
@@ -9,6 +9,7 @@
 	<link rel="apple-touch-icon" sizes="76x76" href="<?php echo $main->path->url->img; ?>logos/favicons/apple-touch-icon-76x76.png">
 	<link rel="apple-touch-icon" sizes="152x152" href="<?php echo $main->path->url->img; ?>logos/favicons/apple-touch-icon-152x152.png">
 	<link rel="apple-touch-icon" sizes="180x180" href="<?php echo $main->path->url->img; ?>logos/favicons/apple-touch-icon-180x180.png">
+	<meta name="apple-mobile-web-app-title" content="APP_NAME">
 	<link rel="shortcut icon" href="<?php echo $main->path->url->img; ?>logos/favicons/favicon.ico">
 	<link rel="icon" type="image/png" href="<?php echo $main->path->url->img; ?>logos/favicons/favicon-192x192.png" sizes="192x192">
 	<link rel="icon" type="image/png" href="<?php echo $main->path->url->img; ?>logos/favicons/favicon-160x160.png" sizes="160x160">
@@ -18,3 +19,4 @@
 	<meta name="msapplication-TileColor" content="#ffffff">
 	<meta name="msapplication-TileImage" content="<?php echo $main->path->url->img; ?>logos/favicons/mstile-144x144.png">
 	<meta name="msapplication-config" content="<?php echo $main->path->url->img; ?>logos/favicons/browserconfig.xml">
+	<meta name="application-name" content="APP_NAME">
