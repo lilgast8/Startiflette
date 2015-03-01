37df1970f44829971eaa5c7829256d25f39c09d1
LilGast8 <contact@gastonbouchayer.com>
2015-03-01 18:42:10 +0100

    Gulp : misprints

--- gulp/tasks/json-min.js
+++ gulp/tasks/json-min.js
@@ -2,20 +2,22 @@ var gulp		= require( 'gulp' );
 
 var paths		= require( '../utils/paths' );
 
+var fs			= require( 'fs' );
 var plumber		= require( 'gulp-plumber' );
 var jsonminify	= require( 'gulp-jsonminify' );
 
 
 
-gulp.task( 'json-min', ['clean'], function () {
-	
+// gulp.task( 'json-min', ['clean'], function () {
+gulp.task( 'json-min', function () {
+	/*
 	var jsonToMin = [
 		{
-			input		: paths.src.jsonConcatFiles,
+			input	: paths.src.jsonConcatFiles,
 			output	: paths.assets.json
 		},
 		{
-			input		: paths.src.jsJsFilesFile,
+			input	: paths.src.jsJsFilesFile,
 			output	: paths.assets.js
 		}
 	];
@@ -31,5 +33,44 @@ gulp.task( 'json-min', ['clean'], function () {
 			.pipe( jsonminify() )
 			.pipe( gulp.dest( json.output ) );
 	}
+	*/
+	
+	
+	// gulp.src( paths.src.jsonConcatFiles )
+	// 	.pipe( plumber() )
+	// 	.pipe( jsonminify() )
+	// 	.pipe( gulp.dest( paths.assets.json ) );
+	
+	
+	minifyJsFiles();
 	
 });
+
+
+
+function minifyJsFiles() {
+	// var files = fs.readdirSync( paths.src.jsJsFilesFile );
+	// var data = fs.readFileSync( paths.src.jsJsFilesFile, 'utf8' );
+	// var jsFiles = fs.readFileSync( paths.src.jsJsFilesFile, 'utf8' );
+	
+	// console.log(data);
+	
+	var jsFiles	= require( '../../' + paths.src.jsJsFilesFile );
+	var data	= '{';
+	var jsFile, jsFileName, isArray;
+	
+	for ( var name in jsFiles ) {
+		jsFile		= jsFiles[name];
+		jsFileName	= jsFile.name;
+		
+		isArray = Array.isArray(jsFileName)
+		if (!isArray)
+			jsFileName = '"'+ jsFileName + '"';
+		
+		data += '"'+ name +'":{"name":'+ jsFileName +'}';
+	}
+	
+	data += '}';
+	
+	console.log(data);
+}
