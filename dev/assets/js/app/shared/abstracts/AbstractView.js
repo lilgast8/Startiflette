dffc3644cc49c9e3ef495e3bacdfad8623dc4221
LilGast8 <contact@gastonbouchayer.com>
2015-09-07 19:56:47 +0200

    AbstractController & AbstractView added

---
+++ dev/assets/js/app/shared/abstracts/AbstractView.js
@@ -0,0 +1,125 @@
+
+
+APP.AbstractView = ( function( window ) {
+	
+	
+	function AbstractView() {
+		APP.EventDispatcher.call( this );
+		
+		this.$	= {};
+		this.v	= {};
+		this.o	= {};
+		this.tw	= {};
+		this.tl	= {};
+	}
+	
+	
+	AbstractView.prototype				= Object.create( APP.EventDispatcher.prototype );
+	AbstractView.prototype.constructor	= AbstractView;
+	
+	
+	AbstractView.prototype.initDOM = function() {
+		
+	};
+	
+	
+	AbstractView.prototype.initTl = function() {
+		
+	};
+	
+	
+	AbstractView.prototype.initView = function() {
+		
+	};
+	
+	
+	AbstractView.prototype.showView = function() {
+		
+	};
+	
+	
+	AbstractView.prototype.hideView = function() {
+		
+	};
+	
+	
+	AbstractView.prototype.bindEvents = function() {
+		
+	};
+	
+	
+	AbstractView.prototype.unbindEvents = function() {
+		
+	};
+	
+	
+	AbstractView.prototype.destroyGSAP = function() {
+		/* tween */
+		for(var tween in this.tw) {
+			var tw = this.tw[tween];
+			
+			tw.kill();
+		}
+		
+		/* timeline */
+		for(var timeline in this.tl) {
+			var tl = this.tl[timeline];
+			
+			tl.stop();
+			tl.clear();
+			tl.kill();
+		}
+		
+		this.tl = {};
+		this.tw = {};
+	};
+	
+	
+	AbstractView.prototype.killTween = function(twName) {
+		this.tw[twName].kill();
+		
+		this.tw[twName] = null;
+	};
+	
+	
+	AbstractView.prototype.killTimeline = function(tlName) {
+		this.tl[tlName].stop();
+		this.tl[tlName].clear();
+		this.tl[tlName].kill();
+		
+		this.tl[tlName] = null;
+	};
+	
+	
+	AbstractView.prototype.destroy = function() {
+		this.unbindEvents();
+		
+		this.destroyGSAP();
+		
+		this.$ = {};
+		this.p = {};
+		this.v = {};
+	};
+	
+	
+	AbstractView.prototype.resize = function() {
+		
+	};
+	
+	
+	/*AbstractView.prototype.changePage = function(e) {
+		if(APP.Config.HAS_PUSHSTATE) { // if pushstate supported
+			e.preventDefault();
+			
+			var url = e.currentTarget.href;
+			
+			APP.RoutesManager.goToPage(url);
+		}
+	};*/
+	
+	
+	return AbstractView;
+	
+	
+} ) ( window );
+
---
+++ dev/assets/js/app/shared/abstracts/_ViewPage.class.js
@@ -0,0 +1,127 @@
+
+
+APP.ViewPage = (function(window) {
+	
+	
+	function ViewPage() {
+		APP.View.call(this);
+		
+		this.name = null;
+		
+		this.E = {
+		//	LOADED : 'loaded',
+		//	INIT : 'init',
+			SHOWN : 'shown',
+			HIDDEN : 'hidden'
+		};
+	}
+	
+	
+	ViewPage.prototype = Object.create(APP.View.prototype);
+	ViewPage.prototype.constructor = ViewPage;
+	
+	
+	ViewPage.prototype.load = function(pageUrl) {
+		this.initTransitionValues();
+		
+		var urlPage = APP.Config.MULTI_LANG ? 
+						APP.Config.WEB_ROOT + APP.Config.LANG + '/ajax-content/' + pageUrl : 
+						APP.Config.WEB_ROOT + 'ajax-content/'+ pageUrl;
+		
+		$.ajax({
+			context		: this,
+			url			: urlPage,
+			type		: 'POST',
+			data		: { ajax:pageUrl },	// useful if need a different behavior on PHP file when AJAX load
+									 		// can be detected with if(isset($_POST['ajax']))
+			dataType	: 'html',
+			success		: this.loaded,
+			error		: this.error
+		});
+	};
+	
+	
+	ViewPage.prototype.loaded = function(data) {
+		this.v.data = data;
+		
+		this.v.isAjaxLoaded = true;
+		
+		this.checkInit();
+	};
+	
+	
+	ViewPage.prototype.error = function() {
+	//	console.log('ajax load error');
+		window.location.href = APP.Config.WEB_ROOT+APP.RoutesManager.pageUrl;
+	};
+	
+	
+	ViewPage.prototype.transitionEnded = function() {
+		this.v.isTransitionHideEnded = true;
+		
+		this.checkInit();
+	};
+	
+	
+	ViewPage.prototype.checkInit = function() {
+		if(this.v.isAjaxLoaded && this.v.isTransitionHideEnded) {
+			APP.RoutesManager.updateGA(); // update Google Analytics
+			
+			APP.Main.$.pageContainer[0].innerHTML = this.v.data;
+			
+			this.v.data = null;
+			
+			this.initTransitionValues();
+			
+			this.init();
+			this.show();
+		}
+	};
+	
+	
+	ViewPage.prototype.initTransitionValues = function() {
+		this.v.isAjaxLoaded				= false;
+		this.v.isTransitionHideEnded	= false;
+	};
+	
+	
+	ViewPage.prototype.initEl = function() {
+		
+	};
+	
+	
+	ViewPage.prototype.bindEvents = function() {
+		
+	};
+	
+	
+	ViewPage.prototype.unbindEvents = function() {
+		
+	};
+	
+	
+	ViewPage.prototype.resize = function() {
+		
+	};
+	
+	
+	ViewPage.prototype.show = function() {
+		this.tw.showPage = TweenLite.to(APP.Main.$.pageContainer, 0.8, {opacity:1, ease:Quad.easeOut, onComplete:function(){
+			this.dispatch(this.E.SHOWN);
+		}.bind(this)});
+	};
+	
+	
+	ViewPage.prototype.hide = function() {
+		this.tw.hidePage = TweenLite.to(APP.Main.$.pageContainer, 0.8, {opacity:0, ease:Quad.easeOut, onComplete:function(){
+			this.destroy();
+			this.dispatch(this.E.HIDDEN);
+		}.bind(this)});
+	};
+	
+	
+	return ViewPage;
+	
+	
+})(window);
+
