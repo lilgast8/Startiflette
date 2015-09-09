7bc5d0934c0c25d572a2c47805b964471de57710
LilGast8 <contact@gastonbouchayer.com>
2015-09-09 12:15:16 +0200

    old browser: structure reviewed

--- dev/server/views/alt/old-browser.php
+++ dev/server/views/alt/old-browser.php
@@ -6,22 +6,27 @@
 		<p><?php echo $this->contents->alt->oldB->desc; ?></p>
 		<div class="browsers">
 			<a href="http://www.google.com/chrome" target="_blank" class="chrome">
+				<img src="<?php echo Path::$URL->img; ?>logos/browsers/browser_chrome.png" alt="Chrome" class="alt-logo">
 				<div class="alt-subtitle">Chrome</div>
 				<p><?php echo $this->contents->alt->oldB->download; ?></p>
 			</a>
 			<a href="http://www.mozilla.org/firefox" target="_blank" class="firefox">
+				<img src="<?php echo Path::$URL->img; ?>logos/browsers/browser_firefox.png" alt="Firefox" class="alt-logo">
 				<div class="alt-subtitle">Firefox</div>
 				<p><?php echo $this->contents->alt->oldB->download; ?></p>
 			</a>
 			<a href="http://www.apple.com/safari" target="_blank" class="safari">
+				<img src="<?php echo Path::$URL->img; ?>logos/browsers/browser_safari.png" alt="Safari" class="alt-logo">
 				<div class="alt-subtitle">Safari</div>
 				<p><?php echo $this->contents->alt->oldB->download; ?></p>
 			</a>
 			<a href="http://www.opera.com/download" target="_blank" class="opera">
+				<img src="<?php echo Path::$URL->img; ?>logos/browsers/browser_opera.png" alt="Opera" class="alt-logo">
 				<div class="alt-subtitle">Opera</div>
 				<p><?php echo $this->contents->alt->oldB->download; ?></p>
 			</a>
 			<a href="http://www.microsoft.com/france/windows/internet-explorer" target="_blank" class="ie">
+				<img src="<?php echo Path::$URL->img; ?>logos/browsers/browser_internet_explorer.png" alt="Internet Explorer" class="alt-logo">
 				<div class="alt-subtitle">Internet Explorer</div>
 				<p><?php echo $this->contents->alt->oldB->download; ?></p>
 			</a>
