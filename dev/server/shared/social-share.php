edbca00051fec3dcd25e132fbc27f279147db2d2
LilGast8 <contact@gastonbouchayer.com>
2015-10-23 17:09:13 +0200

    Social share path fix

--- dev/server/shared/social-share.php
+++ dev/server/shared/social-share.php
@@ -4,12 +4,12 @@
 	<meta property="og:site_name" content="<?php echo Contents::$datas->global->fb->title; ?>" />
 	<meta property="og:description" content="<?php echo Contents::$datas->global->fb->desc; ?>" />
 	<meta property="og:url" content="<?php echo Path::$URL->base; ?>" />
-	<meta property="og:image" content="<?php echo Path::$URL->img; ?>logos/share_facebook.jpg" />
+	<meta property="og:image" content="<?php echo Path::$URL->img; ?>logos/share-facebook.jpg" />
 	<meta property="og:type" content="website" />
 	<!-- Google Plus -->
 	<meta itemprop="name" content="<?php echo Contents::$datas->global->gp->title; ?>" />
 	<meta itemprop="description" content="<?php echo Contents::$datas->global->gp->desc; ?>" />
-	<meta itemprop="image" content="<?php echo Path::$URL->img; ?>logos/share_facebook.jpg" />
+	<meta itemprop="image" content="<?php echo Path::$URL->img; ?>logos/share-facebook.jpg" />
 	<!-- Twitter -->
 	<meta name="twitter:card" content="summary" />
 	<meta name="twitter:title" content="<?php echo Contents::$datas->global->tw->title; ?>" />
@@ -17,4 +17,4 @@
 	<meta name="twitter:site" content="<?php echo Contents::$datas->global->tw->id_site; ?>" />
 	<meta name="twitter:creator" content="<?php echo Contents::$datas->global->tw->id_creator; ?>" />
 	<meta name="twitter:url" content="<?php echo Path::$URL->base; ?>" />
-	<meta name="twitter:image" content="<?php echo Path::$URL->img; ?>divers/share_twitter.jpg" />
+	<meta name="twitter:image" content="<?php echo Path::$URL->img; ?>logos/share-twitter.jpg" />
