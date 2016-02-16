f3067331b0c88f13b6e1a41b442f8c564905311f
LilGast8 <contact@gastonbouchayer.com>
2016-02-16 13:00:18 +0100

    semi-colon added after Path::$URL->img

--- dev/server/views/desktop/pages/error-404.php
+++ dev/server/views/desktop/pages/error-404.php
@@ -6,7 +6,7 @@
 	<?php echo Contents::$datas->error404->title; ?><br>
 	desktop page content / <?php echo Lang::$LANG; ?>
 	<br><br>
-	<img src="<?php echo Path::$URL->img ?>temp/404.jpg" alt="404">
+	<img src="<?php echo Path::$URL->img; ?>temp/404.jpg" alt="404">
 	<br><br><br><br>
 	
 </section>
