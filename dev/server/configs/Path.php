9e4184ba284dfd675ff8803f77bf6c1da6e4740b
LilGast8 <contact@gastonbouchayer.com>
2016-03-31 17:21:37 +0200

    Path.php: path updated

--- dev/server/configs/Path.php
+++ dev/server/configs/Path.php
@@ -56,13 +56,15 @@ class Path
 		self::$URL->base		= Config::$ENVS->{ Config::$ENV }->base_url;
 		self::$URL->assets		= self::$URL->base		. 'assets/';
 		self::$URL->css			= self::$URL->assets	. 'css/';
+		self::$URL->favicons	= self::$URL->assets	. 'favicons/';
+		self::$URL->files		= self::$URL->assets	. 'files/';
 		self::$URL->img			= self::$URL->assets	. 'img/';
 		self::$URL->js			= self::$URL->assets	. 'js/';
 		self::$URL->json		= self::$URL->assets	. 'json/';
+		self::$URL->sounds		= self::$URL->assets	. 'sounds/';
 		self::$URL->svg			= self::$URL->assets	. 'svg/';
 		self::$URL->svgSprite	= self::$URL->svg		. '_sprite/';
 		self::$URL->videos		= self::$URL->assets	. 'videos/';
-		self::$URL->routes		= self::$URL->json		. 'routes/';
 		self::$URL->configs		= self::$URL->base		. 'configs/';
 		self::$URL->server		= self::$URL->base		. 'server/';
 		
@@ -72,9 +74,14 @@ class Path
 		
 		self::$FILE->assets			= 'assets/';
 		self::$FILE->css			= self::$FILE->assets	. 'css/';
+		self::$FILE->favicons		= self::$FILE->assets	. 'favicons/';
+		self::$FILE->files			= self::$FILE->assets	. 'files/';
 		self::$FILE->img			= self::$FILE->assets	. 'img/';
 		self::$FILE->js				= self::$FILE->assets	. 'js/';
 		self::$FILE->json			= self::$FILE->assets	. 'json/';
+		self::$FILE->sounds			= self::$FILE->assets	. 'sounds/';
+		self::$FILE->svg			= self::$FILE->assets	. 'svg/';
+		self::$FILE->videos			= self::$FILE->assets	. 'videos/';
 		self::$FILE->configs		= 'configs/';
 		self::$FILE->jsFilesFile	= self::$FILE->configs	. 'config/js-files.json';
 		self::$FILE->contentsFile	= self::$FILE->configs	. 'config/contents.json';
