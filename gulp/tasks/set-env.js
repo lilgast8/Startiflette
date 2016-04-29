6f21c7a9dfce485f5ac6af6edbd70dd2bbf2e53c
LilGast8 <contact@gastonbouchayer.com>
2016-04-29 16:15:32 +0200

    Improve set-env task to keep only the concerned env in prod

--- gulp/tasks/set-env.js
+++ gulp/tasks/set-env.js
@@ -9,8 +9,9 @@ var fs		= require( 'fs' );
 
 gulp.task( 'set-env', [ 'htaccess' ], function() {
 	
-	var config	= require( '../../' + paths.env.prod + paths.configs.config.configFile );
+	var config	= require( '../../' + paths.env.dev + paths.configs.config.configFile );
 	config.ENV	= options.env;
+	config.ENVS	= '{"' + options.env + '": ' + JSON.stringify( config.ENVS[ options.env ] ) + '}';
 	var data	= JSON.stringify( config );
 	
 	fs.writeFileSync( paths.env.prod + paths.configs.config.configFile, data, 'utf8' );
