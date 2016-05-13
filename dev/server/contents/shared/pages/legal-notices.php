2b7ec1b55fae9549e9e24e7ef5863179507a9d2f
LilGast8 <contact@gastonbouchayer.com>
2016-05-13 20:23:16 +0200

    Add legal notices content

---
+++ dev/server/contents/shared/pages/legal-notices.php
@@ -0,0 +1,24 @@
+<?php
+
+
+
+class LegalNoticesContent extends AbstractContent
+{
+	
+	public function setDatas()
+	{
+		$d = new stdClass();
+		
+		
+		$d->title = "— Mentions légales —";
+		
+		
+		
+		$this->datas = $d;
+	}
+	
+}
+
+
+
+?>
