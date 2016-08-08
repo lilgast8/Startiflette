e86e1643adeafc6cba06b9aca9b6589599d8fc98
LilGast8 <contact@gastonbouchayer.com>
2016-08-08 16:46:22 +0200

    📋 Add Utils.Array

---
+++ dev/assets/js/app/shared/utils/Array.js
@@ -0,0 +1,31 @@
+
+
+WLB.Utils = WLB.Utils || {};
+
+
+WLB.Utils.Array = ( function( window ) {
+	'use strict';
+	
+	
+	/**
+	 * Insert value(s) in a array
+	 * @params {array} array: array where the value(s) will be inserted
+	 * @params {int} index: index of the array
+	 * @params {string, number, int or array} item: value or array of values
+	 */
+	window.STF_arr_insert = function( array, index, item ) {
+		if ( typeof item != 'object' )
+			array.splice( index, 0, item );
+		
+		else
+			item.map( function( value, i ) {
+				return array.splice( index + i, 0, value );
+			} );
+		
+		
+		return array;
+	};
+	
+	
+} ) ( window );
+
