73cef8fc690651ae78a47a7d002c64b5a0708ec7
LilGast8 <contact@gastonbouchayer.com>
2016-10-10 16:13:08 +0200

    ✏️ Fix function/var name in view-name-content.php

--- base/view-name-content.php
+++ base/view-name-content.php
@@ -5,7 +5,7 @@
 class ViewNameContent extends AbstractContent
 {
 	
-	public function setDatas()
+	public function setData()
 	{
 		$d = new stdClass();
 		
@@ -19,7 +19,7 @@ class ViewNameContent extends AbstractContent
 		
 		
 		
-		$this->datas = $d;
+		$this->data = $d;
 	}
 	
 }
