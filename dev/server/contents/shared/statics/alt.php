581fad418b0a321072388b5a939e09e094f14ebc
LilGast8 <contact@gastonbouchayer.com>
2018-03-14 17:19:21 +0100

    🔧 Fix var name

--- dev/server/contents/shared/statics/alt.php
+++ dev/server/contents/shared/statics/alt.php
@@ -10,17 +10,17 @@ class AltContent extends AbstractContent
 		$d = new stdClass();
 		
 		
-		$d->noJS		= new stdClass();
+		$d->noJS			= new stdClass();
 		
-		$d->noJS->title	= "JavaScript is disabled.";
-		$d->noJS->desc	= "To view this web page, please enable JavaScript in your browser preferences. <br/>Once Javascript is enabled, please reload the web page.";
+		$d->noJS->title		= "JavaScript is disabled.";
+		$d->noJS->desc		= "To view this web page, please enable JavaScript in your browser preferences. <br/>Once Javascript is enabled, please reload the web page.";
 		
 		
-		$d->oldB		= new stdClass();
+		$d->oldB			= new stdClass();
 		
-		$d->oldB->title	= "Your browser is obsolete.";
-		$d->oldB->desc	= "Please upgrade it for free to improve its performance <br />and so have a better comfort of browsing.";
-		$d->oldB->dl	= "Download";
+		$d->oldB->title		= "Your browser is obsolete.";
+		$d->oldB->desc		= "Please upgrade it for free to improve its performance <br />and so have a better comfort of browsing.";
+		$d->oldB->download	= "Download";
 		
 		
 		
